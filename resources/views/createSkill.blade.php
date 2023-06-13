c799f4f62c3c9289b5da540ae01986fbd43b7912
Suren Ispiryan <surenispiryan@Surens-MacBook-Pro.local>
2023-06-13 13:57:58 +0400

    debug

--- resources/views/createSkill.blade.php
+++ resources/views/createSkill.blade.php
@@ -18,36 +18,41 @@
         <a href="{{ url('/') }}">
             <button class="btn btn-primary">Back</button>
         </a>
-        <form class="form" action="/create-skill" method="POST">
-            @csrf
-            <label for="cars">Choose a specialization:</label>
-            <select name="specializations">
-                 @foreach ($specializations as $specialization)
-                    <option value={{ $specialization->id }}>{{ $specialization->specialization_eng }}</option>
-                @endforeach
-            </select>
 
-            <input
-                class="form-control inputs"
-                name="eng"
-                placeholder="Skill"
-            />
-            <input
-                class="form-control inputs"
-                name="rus"
-                placeholder="навык"
-            />
-            <input
-                class="form-control inputs"
-                name="arm"
-                placeholder="հմտություն"
-            />
-            <button
-                type="submit"
-                class="form-control btn btn-success"
-            >
-                create
-            </button>
-        </form>
+        @if ($specializations->count() > 0 )
+            <form class="form" action="/create-skill" method="POST">
+                @csrf
+                <label for="cars">Choose a specialization:</label>
+                <select name="specializations">
+                    @foreach ($specializations as $specialization)
+                        <option value={{ $specialization->id }}>{{ $specialization->specialization_eng }}</option>
+                    @endforeach
+                </select>
+
+                <input
+                    class="form-control inputs"
+                    name="eng"
+                    placeholder="Skill"
+                />
+                <input
+                    class="form-control inputs"
+                    name="rus"
+                    placeholder="навык"
+                />
+                <input
+                    class="form-control inputs"
+                    name="arm"
+                    placeholder="հմտություն"
+                />
+                <button
+                    type="submit"
+                    class="form-control btn btn-success"
+                >
+                    create
+                </button>
+            </form>
+        @else
+            <p>First create specialization</p>
+        @endif
     </body>
 </html>
