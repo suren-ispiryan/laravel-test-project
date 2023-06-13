9ec5e13e049aeb090dc86ebbffbb591c48e81058
Suren Ispiryan <surenispiryan@Surens-MacBook-Pro.local>
2023-06-13 16:10:31 +0400

    styling

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,41 +16,46 @@
     </head>
     <body class="antialiased">
         <a href="{{ url('/specialization') }}">
-            <button class="btn-primary">Create specializations</button>
+            <button class="btn btn-primary">Create specializations</button>
         </a>
         <a href="{{ url('/skill') }}">
-            <button class="btn-primary">Create skills</button>
+            <button class="btn btn-primary">Create skills</button>
         </a>
-
         @if ($allSpecifications->count() > 0 )
-            <table>
+            <table class="table table-sm">
+                <thead>
                 <tr>
                 <th>Specializations</th>
                 <th>Skills</th>
+                <th>Delete</th>
                 </tr>
-                @foreach ($allSpecifications as $specification)
-                    <tr>
-                        <td>
-                            <p> {{ $specification->specialization_eng }}</p>
-                            <p> {{ $specification->specialization_rus }}</p>
-                            <p> {{ $specification->specialization_arm }}</p>
-                        </td>
-                        <td>
-                            @foreach ($specification->skill as $skill)
-                                <p> {{ $skill->skill_eng }}</p>
-                                <p> {{ $skill->skill_rus }}</p>
-                                <p> {{ $skill->skill_arm }}</p>
-                            @endforeach
-                        </td>
-                        <td>
-                            <a href="{{ url('/delete/'.$specification->id) }}">
-                            <button class="btn btn-danger">Delete</button>
-                            </a>
-                        </td>
-                    </tr>
-                @endforeach
+                </thead>
+                <tbody>
+                    @foreach ($allSpecifications as $specification)
+                        <tr>
+                            <td>
+                                <p> {{ $specification->specialization_eng }}</p>
+                                <p> {{ $specification->specialization_rus }}</p>
+                                <p> {{ $specification->specialization_arm }}</p>
+                            </td>
+                            <td>
+                                @foreach ($specification->skill as $skill)
+                                    <div>
+                                        {{ $skill->skill_eng }}
+                                        {{ $skill->skill_rus }}
+                                        {{ $skill->skill_arm }}
+                                    </div>
+                                @endforeach
+                            </td>
+                            <td>
+                                <a href="{{ url('/delete/'.$specification->id) }}">
+                                <button class="btn btn-danger">Delete</button>
+                                </a>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
             </table>
         @endif
-
     </body>
 </html>
